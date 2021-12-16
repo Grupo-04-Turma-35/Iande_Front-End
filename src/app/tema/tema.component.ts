c0529d0231774273c76b7ef1b041ed4e17f56e19
Helen Lissa Sonoda <helen.lissa@gmail.com>
2021-12-16 00:48:04 -0300

    Ajustes no front-end

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -26,6 +26,13 @@ export class TemaComponent implements OnInit {
       this.router.navigate(['/entrar']);
     }
 
+    if (environment.tipo != 'adm') {
+      this.alertas.showAlertInfo(
+        'Você precisa ser administrador para acessar essa rota!'
+      );
+      this.router.navigate(['/inicio']);
+    }
+
     this.findAllTemas();
   }
 
