51c3ce3413e73e9284e89bfef630cbfad6d4c787
Helen Lissa Sonoda <helen.lissa@gmail.com>
2021-12-19 23:01:07 -0300

    Estilização da Home

---
+++ src/app/rodape-home/rodape-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rodape-home',
+  templateUrl: './rodape-home.component.html',
+  styleUrls: ['./rodape-home.component.css']
+})
+export class RodapeHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
